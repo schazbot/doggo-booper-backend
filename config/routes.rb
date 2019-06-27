d7d924b956c4c2ac6d6c0a553bbab98cc9bec86d
schazbot <charlotte.pearce1984@gmail.com>
2019-06-27 17:54:57 +0100

    added uploaded  widget

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index]
-  resources :dogs, only: [:index, :create, :destroy]
+  resources :dogs, only: [:index, :create, :destroy, :update]
   resources :user_dogs, only: [:index, :create, :show]
 end
