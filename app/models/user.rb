be5459489c2a446ec27735642a94689aa1201d02
schazbot <charlotte.pearce1984@gmail.com>
2019-07-17 17:57:40 +0100

    added routes and auth controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :user_dogs
     has_many :dogs, through: :user_dogs
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:index]
+  resources :users, only: [:index, :create]
   resources :dogs, only: [:index, :create, :destroy, :update]
   resources :user_dogs, only: [:index, :create, :show]
+
+  post "/auth/create", to: "auth#create"
+  
+  get "/my_dogs", to: "dogs#my_dogs"
 end
