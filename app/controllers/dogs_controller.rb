55d99001c850067cfa856d2471e2b670933d55e0
schazbot <charlotte.pearce1984@gmail.com>
2019-11-12 11:26:46 +0000

    update dog returning object

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -11,8 +11,10 @@ class DogsController < ApplicationController
     end 
 
     def update
+        # byebug
         dog = Dog.find(params[:id])
         dog.update(boops: params[:boops])
+        render json: dog
     end 
 
 
@@ -20,5 +22,10 @@ class DogsController < ApplicationController
         dog = Dog.find(params[:id])
         dog.destroy
     end
+    # private
+    
+    # def dog_params
+    #     params.require(:dog).permit(:name, :url, :boops)
+    # end
 
 end
